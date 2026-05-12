febef05b3b83baf9e130c0f59561ed5233348919
Fatimah Azzahra <youecan016@gmail.com>
2026-05-12 15:15:31 +0700

    fix: force https for assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,24 +1,20 @@
-<?php
-
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // Jangan lupa tambahkan baris ini!
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // Tambahkan logika ini
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
